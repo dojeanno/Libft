238a5cf170b8dceb4d6e699d27fa98926cce844d
Dominic Jeannotte-parent <dojeanno@c1r1p1.42quebec.com>
2023-02-13 12:56:58 -0500

    ft_atoi , lower upper

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: dojeanno <dojeanno@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/13 09:29:17 by dojeanno          #+#    #+#             */
-/*   Updated: 2023/02/13 12:27:38 by dojeanno         ###   ########.fr       */
+/*   Updated: 2023/02/13 12:56:28 by dojeanno         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,4 +29,8 @@ void	*ft_memset(void *b, int c, size_t len);
 void	ft_bzero(void *s, size_t n);
 void	*ft_memcpy(void *dst, const void *src, size_t n);
 void	*ft_memmove(void *dst, const void *src, size_t len);
+int		ft_tolower(int c);
+int		ft_toupper(int c);
+int		ft_atoi(const char *str);
+
 #endif
